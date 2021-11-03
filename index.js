0ca5aa9aaa724736174023a695acd2f81bf5c892
Henrry Esposito <henrrygesposito@gmail.com>
2021-11-03 00:43:43 -0300

    feat: new request endpoint

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-import express, { json } from 'express'
-import pedidosRouter from './routes/pedidos.js'
-import {promises as fs} from 'fs';
+import express, { json } from 'express';
+import { promises as fs } from 'fs';
+import pedidosRouter from './routes/pedidos.js';
 
-const {readFile, writeFile} = fs;
+const { readFile, writeFile } = fs;
 const app = express();
 const port = 3000;
-const data = JSON.parse(await readFile("pedidos.json"));
+// const data = JSON.parse(await readFile("pedidos.json"));
 
 app.use(express.json());
 
-app.use('/account', pedidosRouter);
+app.use('/pedidos', pedidosRouter);
 
 app.get('/', (req, res) => {
-    res.send("test");
-})
-
+  res.send('test');
+});
 
 app.listen(port, async () => {
-    console.log("API started");
+  console.log('API started');
 });
--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,10 +1,24 @@
-import { Router } from "express";
+import { Router } from 'express';
+import { promises as fs } from 'fs';
 
+const { readFile, writeFile } = fs;
 const router = Router();
 
-router.post('/pedido', (req, res) => {
-    var pedidoNovo = {
-    }
-})
+router.post('/novo', async (req, res) => {
+  const pedidoNovo = req.body;
+  const data = JSON.parse(await readFile('pedidos.json'));
+  const { nextId, pedidos } = data;
+
+  pedidoNovo.id = nextId;
+  pedidoNovo.timestamp = new Date().toISOString();
+  pedidoNovo.entregue = false;
+
+  data.nextId += 1;
+  data.pedidos.push(pedidoNovo);
+
+  writeFile('pedidos.json', JSON.stringify(data));
+
+  res.send(pedidoNovo);
+});
 
 export default router;
